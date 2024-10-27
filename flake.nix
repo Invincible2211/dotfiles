073a928b1f4a8fd1001049b9489d397e2db02ad7
Invincible2211 <58395689+Invincible2211@users.noreply.github.com>
2024-10-27 16:04:14 +0100

    Jesse will das so :(

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Mach ich später, versprochen!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+
+  nixosConfigurations.kaguya = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      ./configuration.nix
+      home-manager.nixosModules.home-manager
+    ];
+    specialArgs.username = "fynn";
+  };
+
+  };
+}
