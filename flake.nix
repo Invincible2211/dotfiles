a546ca4793223599d9ce9a9b7bd31cc7cb9f1773
anders130 <93037023+anders130@users.noreply.github.com>
2025-01-06 10:15:37 +0100

    nix fmt

--- flake.nix
+++ flake.nix
@@ -1,58 +1,58 @@
 {
-    description = "Mach ich später, versprochen!";
+  description = "Mach ich später, versprochen!";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-        home-manager = {
-            url = "github:nix-community/home-manager/release-24.11";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        nur.url = "github:nix-community/NUR";
-
-        modulix = {
-            url = "github:anders130/modulix";
-            inputs.nixpkgs.follows = "nixpkgs";
-            inputs.home-manager.follows = "home-manager";
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    nur.url = "github:nix-community/NUR";
 
-    outputs = inputs: {
-        formatter.x86_64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.alejandra;
-        nixosConfigurations = inputs.modulix.lib.mkHosts {
-            inherit inputs;
-            flakePath = "/home/jesse/Projects/invincible-dotfiles";
-            modulesPath = ./modules;
-            helpers = import ./lib inputs;
-            specialArgs = {
-                hostname = "nixos";
-            };
-            sharedConfig = {
-                imports = [
-                    inputs.home-manager.nixosModules.home-manager
-                    inputs.nur.nixosModules.nur
-                ];
-                modules = {
-                    programms = {
-                        common.enable = true;
-                        firefox.enable = true;
-                        kitty.enable = true;
-                    };
-                    services.printing.enable = true;
-                    system = {
-                        audio.enable = true;
-                        display.enable = true;
-                        font.enable = true;
-                        gnome.enable = true;
-                        localisation.enable = true;
-                        network.enable = true;
-                        nix.enable = true;
-                        shell = {
-                            alias.enable = true;
-                        };
-                        user.enable = true;
-                    };
-                };
-            };
-        };
+    modulix = {
+      url = "github:anders130/modulix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
+  };
+
+  outputs = inputs: {
+    formatter.x86_64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    nixosConfigurations = inputs.modulix.lib.mkHosts {
+      inherit inputs;
+      flakePath = "/home/jesse/Projects/invincible-dotfiles";
+      modulesPath = ./modules;
+      helpers = import ./lib inputs;
+      specialArgs = {
+        hostname = "nixos";
+      };
+      sharedConfig = {
+        imports = [
+          inputs.home-manager.nixosModules.home-manager
+          inputs.nur.nixosModules.nur
+        ];
+        modules = {
+          programms = {
+            common.enable = true;
+            firefox.enable = true;
+            kitty.enable = true;
+          };
+          services.printing.enable = true;
+          system = {
+            audio.enable = true;
+            display.enable = true;
+            font.enable = true;
+            gnome.enable = true;
+            localisation.enable = true;
+            network.enable = true;
+            nix.enable = true;
+            shell = {
+              alias.enable = true;
+            };
+            user.enable = true;
+          };
+        };
+      };
+    };
+  };
 }
