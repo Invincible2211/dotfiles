dce1cb5aa43438716589a202d837dedfca18b04e
Invincible2211 <58395689+Invincible2211@users.noreply.github.com>
2025-01-20 10:26:12 +0100

    add new shell aliases

--- modules/system/shell/alias.nix
+++ modules/system/shell/alias.nix
@@ -1,10 +1,25 @@
 {
   username,
   internalName,
+  flakePath,
   ...
 }: {
+  programs.nh = {
+    enable = true;
+    flake = flakePath;
+  };
   environment.shellAliases = {
-    rebuild = "sudo nixos-rebuild switch --flake /home/${username}/dotfiles/.#${internalName}";
+    rebuild = "nh os switch -H ${internalName} -- ";
     nfmt = "nix fmt -I /home/${username}/dotfiles";
+    #gnome
+    watch = "dconf watch /";
+    gnreset = "dconf reset -f /";
+    #git commands
+    gadd = "";
+    gadda = "git add --all";
+    gc = "git commit";
+    gpush = "git push";
+    gpull = "git pull";
+    gfetch = "git fetch";
   };
 }
